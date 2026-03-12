ba121540f42d097cfd31cb625601987b48807100
Davidlaizz <1830579068@qq.com>
2026-03-12 15:56:10 +0800

    技巧题：异或

---
+++ src/main/java/LC136_skill.java
@@ -0,0 +1,10 @@
+public class LC136_skill {
+    public int singleNumber(int[] nums) {
+        int res = 0;
+        for (int num : nums) {
+            res ^= num;
+        }
+        return res;
+    }
+
+}
